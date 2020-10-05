5aaa9918aae4ffcf129c6c7fcc74cace377e744c
Sardor <mr.narzullaev@gmail.com>
2020-10-05 21:52:06 +0800

    minor change

--- lib/story.dart
+++ lib/story.dart
@@ -8,6 +8,8 @@ class Story {
   String _choice2;
 
 
+
+
   //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
   Story({storyTitle: String, choice1: String, choice2: String});
 
